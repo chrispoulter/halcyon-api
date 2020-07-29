bb32e1d9316233ee7f555a18476638e3421fb09e
Chris Poulter <cpoulter@hotmail.co.uk>
2020-07-29 10:51:12 +0100

    updated password service

--- Halcyon.Web/Services/Password/PasswordService.cs
+++ Halcyon.Web/Services/Password/PasswordService.cs
@@ -1,11 +1,47 @@
-﻿namespace Halcyon.Web.Services.Password
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace Halcyon.Web.Services.Password
 {
     public class PasswordService : IPasswordService
     {
-        public string GenerateHash(string str) 
-            => BCrypt.Net.BCrypt.HashPassword(str, 10);
+        private const int SaltSize = 16;
 
-        public bool VerifyHash(string str, string hash) 
-            => BCrypt.Net.BCrypt.Verify(str, hash);
+        private const int KeySize = 32;
+
+        private const int Iterations = 10000;
+
+        public string GenerateHash(string str)
+        {
+            using var algorithm = new Rfc2898DeriveBytes(
+               str,
+               SaltSize,
+               Iterations,
+               HashAlgorithmName.SHA256);
+
+            var key = Convert.ToBase64String(algorithm.GetBytes(KeySize));
+            var salt = Convert.ToBase64String(algorithm.Salt);
+
+            return $"{salt}.{key}";
+        }
+
+        public bool VerifyHash(string str, string hash)
+        {
+            var parts = hash.Split('.', 2);
+            var salt = Convert.FromBase64String(parts[0]);
+            var key = Convert.FromBase64String(parts[1]);
+
+            using var algorithm = new Rfc2898DeriveBytes(
+              str,
+              salt,
+              Iterations,
+              HashAlgorithmName.SHA256);
+
+            var keyToCheck = algorithm.GetBytes(KeySize);
+            var verified = keyToCheck.SequenceEqual(key);
+
+            return verified;
+        }
     }
 }
