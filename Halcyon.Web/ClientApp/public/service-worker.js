21984c5e26a8bae1cc9dca0f268ac2b0fd2fe902
Chris Poulter <cpoulter@hotmail.co.uk>
2020-12-21 10:16:27 +0000

    updated service worker

--- Halcyon.Web/ClientApp/public/service-worker.js
+++ Halcyon.Web/ClientApp/public/service-worker.js
@@ -9,7 +9,16 @@ workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 workbox.routing.registerRoute(
     /^https:\/\/fonts\.googleapis\.com/,
     new workbox.strategies.StaleWhileRevalidate({
-        cacheName: 'google-fonts-stylesheets'
+        cacheName: 'google-fonts-stylesheets',
+        plugins: [
+            new workbox.cacheableResponse.Plugin({
+                statuses: [0, 200]
+            }),
+            new workbox.expiration.Plugin({
+                maxAgeSeconds: 60 * 60 * 24 * 365,
+                maxEntries: 30
+            })
+        ]
     })
 );
 
