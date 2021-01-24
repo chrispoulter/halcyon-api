8fce2de1d844f36362c43499f2ba6a460a0f3e5d
Chris Poulter <cpoulter@hotmail.co.uk>
2021-01-24 13:22:03 +0000

    styling update

--- Halcyon.Web/ClientApp/src/index.js
+++ Halcyon.Web/ClientApp/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import { App } from './App';
-import './index.scss';
+import './styles/index.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
