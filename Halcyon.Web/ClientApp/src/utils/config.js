81f0745a91cb5a5adc5a52eabe75180f98303602
Chris Poulter <cpoulter@hotmail.co.uk>
2021-07-16 09:25:56 +0100

    updated config

--- Halcyon.Web/ClientApp/src/utils/config.js
+++ Halcyon.Web/ClientApp/src/utils/config.js
@@ -1,6 +1,6 @@
 export const config = {
+    VERSION: process.env.REACT_APP_VERSION || '1.0.0',
     STAGE: process.env.REACT_APP_STAGE || 'local',
-    VERSION: process.env.REACT_APP_VERSION || '1.0.0-local',
     GA_MEASUREMENTID: process.env.REACT_APP_GA_MEASUREMENTID,
     API_URL: process.env.REACT_APP_API_URL || '/api'
 };
