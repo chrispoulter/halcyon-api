62068636e142198c8a6b88d3f7d2e91aa2e82ee2
Chris Poulter <cpoulter@hotmail.co.uk>
2021-08-22 10:50:58 +0100

    updated button spinner

--- Halcyon.Web/ClientApp/src/components/form/Button.js
+++ Halcyon.Web/ClientApp/src/components/form/Button.js
@@ -4,9 +4,9 @@ export const Button = ({ loading, disabled, children, ...rest }) => (
     <button type="button" disabled={loading || disabled} {...rest}>
         {loading ? (
             <>
-                <span className="spinner-border spinner-border-sm"></span>
-                <span className="spinner-border spinner-border-sm"></span>
-                <span className="spinner-border spinner-border-sm"></span>
+                <span className="spinner-grow spinner-grow-sm"></span>
+                <span className="spinner-grow spinner-grow-sm"></span>
+                <span className="spinner-grow spinner-grow-sm"></span>
             </>
         ) : (
             children
