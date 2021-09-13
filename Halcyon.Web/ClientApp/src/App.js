c15682a26e513f1aeee8a1e84ec3e0a9d8374412
Chris Poulter <cpoulter@hotmail.co.uk>
2021-09-13 10:29:02 +0100

    route refactor

--- Halcyon.Web/ClientApp/src/App.js
+++ Halcyon.Web/ClientApp/src/App.js
@@ -1,31 +1,16 @@
 import React, { Suspense } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import {
     AuthProvider,
     ModalProvider,
     ToastProvider,
     Header,
     Footer,
-    PrivateRoute,
     Spinner,
     Meta,
     ErrorBoundary
 } from './components';
-import {
-    HomePage,
-    NotFoundPage,
-    LoginPage,
-    RegisterPage,
-    ForgotPasswordPage,
-    ResetPasswordPage,
-    MyAccountPage,
-    UpdateProfilePage,
-    ChangePasswordPage,
-    UserPage,
-    CreateUserPage,
-    UpdateUserPage
-} from './pages';
-import { USER_ADMINISTRATOR_ROLES } from './utils/auth';
+import { Routes } from './Routes';
 
 export const App = () => (
     <Suspense fallback={<Spinner />}>
@@ -36,63 +21,7 @@ export const App = () => (
                         <Meta />
                         <Header />
                         <ErrorBoundary>
-                            <Switch>
-                                <Route path="/" component={HomePage} exact />
-                                <Route
-                                    path="/register"
-                                    component={RegisterPage}
-                                    exact
-                                />
-                                <Route
-                                    path="/login"
-                                    component={LoginPage}
-                                    exact
-                                />
-                                <Route
-                                    path="/forgot-password"
-                                    component={ForgotPasswordPage}
-                                    exact
-                                />
-                                <Route
-                                    path="/reset-password/:token"
-                                    component={ResetPasswordPage}
-                                    exact
-                                />
-                                <PrivateRoute
-                                    path="/my-account"
-                                    component={MyAccountPage}
-                                    exact
-                                />
-                                <PrivateRoute
-                                    path="/update-profile"
-                                    component={UpdateProfilePage}
-                                    exact
-                                />
-                                <PrivateRoute
-                                    path="/change-password"
-                                    component={ChangePasswordPage}
-                                    exact
-                                />
-                                <PrivateRoute
-                                    path="/user"
-                                    requiredRoles={USER_ADMINISTRATOR_ROLES}
-                                    component={UserPage}
-                                    exact
-                                />
-                                <PrivateRoute
-                                    path="/user/create"
-                                    requiredRoles={USER_ADMINISTRATOR_ROLES}
-                                    component={CreateUserPage}
-                                    exact
-                                />
-                                <PrivateRoute
-                                    path="/user/:id"
-                                    requiredRoles={USER_ADMINISTRATOR_ROLES}
-                                    component={UpdateUserPage}
-                                    exact
-                                />
-                                <Route component={NotFoundPage} />
-                            </Switch>
+                            <Routes />
                         </ErrorBoundary>
                         <Footer />
                     </ToastProvider>
