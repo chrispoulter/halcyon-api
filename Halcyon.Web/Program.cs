2d4a7706c171bb83d45cd47c4745dfc2dbf811b3
Chris Poulter <cpoulter@hotmail.co.uk>
2022-03-17 12:24:47 +0000

    fix connection string

--- Halcyon.Web/Program.cs
+++ Halcyon.Web/Program.cs
@@ -21,7 +21,7 @@ using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("HalcyonDatabase").Trim('"');
+var connectionString = builder.Configuration["ConnectionStrings:HalcyonDatabase"].Trim('"');
 
 builder.Services.AddDbContext<HalcyonDbContext>(options =>
     options
