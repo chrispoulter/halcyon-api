8797671615dfe0478f029fcde517def02bed672a
Chris Poulter <cpoulter@hotmail.co.uk>
2022-06-07 09:57:32 +0100

    remove workbox

--- Halcyon.Web/ClientApp/workbox-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    globDirectory: 'build/',
-    globPatterns: ['**/*.{json,xml,ico,png,svg,html,js,txt,webmanifest,css}'],
-    globIgnores: ['api/**/*', 'swagger/**/*', 'health/**/*'],
-    swDest: './build/service-worker.js',
-    swSrc: './public/service-worker.js'
-};
