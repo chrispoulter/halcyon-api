e000d1c0e5292f62c8051687a122495b2291643f
Chris Poulter <cpoulter@hotmail.co.uk>
2022-08-08 14:18:16 +0100

    remove service worker

--- Halcyon.Web/ClientApp/src/index.js
+++ Halcyon.Web/ClientApp/src/index.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import * as serviceWorker from './serviceWorker';
 import { App } from './App';
 import './styles/index.scss';
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(<App />);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
