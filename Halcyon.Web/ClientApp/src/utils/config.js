f88cd5555d29fcbca7201c86d1897f53fc825393
Chris Poulter <cpoulter@hotmail.co.uk>
2022-09-30 12:56:05 +0100

    convert to vite

--- Halcyon.Web/ClientApp/src/utils/config.js
+++ Halcyon.Web/ClientApp/src/utils/config.js
@@ -1,4 +1,4 @@
 export const config = {
-    VERSION: process.env.REACT_APP_VERSION || '1.0.0',
-    API_URL: process.env.REACT_APP_API_URL || '/api'
+    VERSION: import.meta.env.VITE_VERSION || '1.0.0',
+    API_URL: import.meta.env.VITE_API_URL || '/api'
 };
