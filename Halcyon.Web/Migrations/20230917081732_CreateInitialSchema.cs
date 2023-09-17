82143e7efe2fcdfe7a3a45c0703983bbe73b433d
Chris Poulter <cpoulter@hotmail.co.uk>
2023-09-17 15:52:44 +0100

    update psql naming

---
+++ Halcyon.Web/Migrations/20230917081732_CreateInitialSchema.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Halcyon.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateInitialSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    email_address = table.Column<string>(type: "text", nullable: false),
+                    password = table.Column<string>(type: "text", nullable: true),
+                    password_reset_token = table.Column<Guid>(type: "uuid", nullable: true),
+                    first_name = table.Column<string>(type: "text", nullable: false),
+                    last_name = table.Column<string>(type: "text", nullable: false),
+                    date_of_birth = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    is_locked_out = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false),
+                    roles = table.Column<string[]>(type: "text[]", nullable: true),
+                    version = table.Column<Guid>(type: "uuid", nullable: false),
+                    search = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_users", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_users_email_address",
+                table: "users",
+                column: "email_address",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
