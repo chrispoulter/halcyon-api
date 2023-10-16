d744dfe60962332e99109b2374fc92ef1dbb7545
Chris Poulter <cpoulter@hotmail.co.uk>
2023-10-16 16:18:53 +0100

    code clean up

--- src/Halcyon.Api/Models/User/SearchUsersResponse.cs
+++ src/Halcyon.Api/Models/User/SearchUsersResponse.cs
@@ -8,4 +8,19 @@
 
         public bool HasPreviousPage { get; set; }
     }
+
+    public class SearchUserResponse
+    {
+        public int Id { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public bool IsLockedOut { get; set; }
+
+        public List<string> Roles { get; set; }
+    }
 }
