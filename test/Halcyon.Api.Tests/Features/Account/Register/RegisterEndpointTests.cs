aca0d4b8f8d1db51519b0cee86be3a4584d57098
Chris Poulter <cpoulter@hotmail.co.uk>
2024-03-21 16:28:56 +0000

    test data clean up

--- test/Halcyon.Api.Tests/Features/Account/Register/RegisterEndpointTests.cs
+++ test/Halcyon.Api.Tests/Features/Account/Register/RegisterEndpointTests.cs
@@ -1,78 +1,78 @@
-using AutoFixture;
-using FluentAssertions;
 using Halcyon.Api.Common;
 using Halcyon.Api.Data;
 using Halcyon.Api.Features.Account.Register;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
-using System.Net;
 using System.Net.Http.Json;
-using System.Net.Mail;
 
 namespace Halcyon.Api.Tests.Features.Account.Register;
 
 public class RegisterEndpointTests : IClassFixture<CustomWebApplicationFactory<Program>>
 {
+    private const string RequestUri = "/account/register";
+
     private readonly WebApplicationFactory<Program> factory;
-    private readonly HttpClient client;
-    private readonly Fixture fixture;
 
     public RegisterEndpointTests(CustomWebApplicationFactory<Program> factory)
     {
         this.factory = factory;
-        this.client = factory.CreateClient();
-        this.fixture = new Fixture();
     }
 
     [Fact]
     public async Task Register_WhenDuplicateEmailAddress_ShouldReturnBadRequest()
     {
-        using var scope = factory.Services.CreateScope();
-        using var dbContext = scope.ServiceProvider.GetRequiredService<HalcyonDbContext>();
+        var user = await CreateTestUser();
+        var request = CreateRegisterRequest(user.EmailAddress);
 
-        var emailAddress = fixture.Create<MailAddress>().Address;
-
-        var user = fixture
-            .Build<User>()
-            .With(p => p.EmailAddress, emailAddress)
-            .With(p => p.DateOfBirth, new DateOnly(1970, 1, 1))
-            .Create();
-
-        dbContext.Users.Add(user);
-
-        await dbContext.SaveChangesAsync();
-
-        var request = fixture
-            .Build<RegisterRequest>()
-            .With(p => p.EmailAddress, user.EmailAddress)
-            .With(p => p.DateOfBirth, user.DateOfBirth)
-            .Create();
-
-        var response = await client.PostAsJsonAsync("/account/register", request);
-        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        var client = factory.CreateClient();
+        var response = await client.PostAsJsonAsync(RequestUri, request);
+        Assert.False(response.IsSuccessStatusCode);
 
         var result = await response.Content.ReadFromJsonAsync<ProblemDetails>();
-        result.Should().NotBeNull();
-        result!.Title.Should().Be("User name is already taken.");
+        Assert.NotNull(result);
+        Assert.Equal("User name is already taken.", result.Title);
     }
 
     [Fact]
     public async Task Register_WhenRequestValid_ShouldCreateNewUser()
     {
-        var emailAddress = fixture.Create<MailAddress>().Address;
+        var request = CreateRegisterRequest();
 
-        var request = fixture
-            .Build<RegisterRequest>()
-            .With(p => p.EmailAddress, emailAddress)
-            .With(p => p.DateOfBirth, new DateOnly(1970, 1, 1))
-            .Create();
-
-        var response = await client.PostAsJsonAsync("/account/register", request);
-        response.EnsureSuccessStatusCode();
+        var client = factory.CreateClient();
+        var response = await client.PostAsJsonAsync(RequestUri, request);
+        Assert.True(response.IsSuccessStatusCode);
 
         var result = await response.Content.ReadFromJsonAsync<UpdateResponse>();
-        result.Should().NotBeNull();
-        result!.Id.Should().BeGreaterThan(0);
+        Assert.NotNull(result);
+        Assert.NotEqual(0, result.Id);
+    }
+
+    private static RegisterRequest CreateRegisterRequest(string? emailAddress = null) =>
+        new()
+        {
+            EmailAddress = emailAddress ?? $"{Guid.NewGuid()}@example.com",
+            Password = "password",
+            FirstName = "Test",
+            LastName = "User",
+            DateOfBirth = new DateOnly(1070, 1, 1)
+        };
+
+    private async Task<User> CreateTestUser()
+    {
+        var user = new User
+        {
+            EmailAddress = $"{Guid.NewGuid()}@example.com",
+            FirstName = "Test",
+            LastName = "User",
+            DateOfBirth = new DateOnly(1070, 1, 1)
+        };
+
+        using var scope = factory.Services.CreateScope();
+        using var dbContext = scope.ServiceProvider.GetRequiredService<HalcyonDbContext>();
+        dbContext.Users.Add(user);
+        await dbContext.SaveChangesAsync();
+
+        return user;
     }
 }
