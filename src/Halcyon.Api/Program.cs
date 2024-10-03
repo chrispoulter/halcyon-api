efdd47eddc5a73bf3d9d3a054524d443c4c5500e
Chris Poulter <cpoulter@hotmail.co.uk>
2024-10-03 11:26:27 +0100

    update swagger

--- src/Halcyon.Api/Program.cs
+++ src/Halcyon.Api/Program.cs
@@ -123,7 +123,7 @@ builder.Services.AddSwaggerGen(options =>
         {
             Version = version,
             Title = "Halcyon API",
-            Description = "A web api template."
+            Description = "A RESTful API project template with a sense of peace and tranquillity"
         }
     );
 
