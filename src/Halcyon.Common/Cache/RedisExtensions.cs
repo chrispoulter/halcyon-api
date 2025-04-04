430e186fe35c9dce6ff36ea191868f9b8151b3df
Chris Poulter <cpoulter@hotmail.co.uk>
2025-04-04 09:10:08 +0100

    update namespaces

--- src/Halcyon.Common/Cache/RedisExtensions.cs
+++ src/Halcyon.Common/Cache/RedisExtensions.cs
@@ -2,7 +2,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Halcyon.Common.Infrastructure;
+namespace Halcyon.Common.Cache;
 
 public static class RedisExtensions
 {
