51a2f7290bea84a05797e37f3685dc2980a9b607
Chris Poulter <cpoulter@hotmail.co.uk>
2025-05-16 10:29:34 +0100

    code clean up

--- Halcyon.Api/Common/Authentication/AuthenticationExtensions.cs
+++ Halcyon.Api/Common/Authentication/AuthenticationExtensions.cs
@@ -32,22 +32,6 @@ public static class AuthenticationExtensions
                         Encoding.UTF8.GetBytes(jwtSettings.SecurityKey)
                     ),
                 };
-
-                options.Events = new JwtBearerEvents
-                {
-                    OnMessageReceived = context =>
-                    {
-                        var accessToken = context.Request.Query["access_token"];
-                        var path = context.HttpContext.Request.Path;
-
-                        if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/hubs"))
-                        {
-                            context.Token = accessToken;
-                        }
-
-                        return Task.CompletedTask;
-                    },
-                };
             });
 
         builder.Services.AddAuthorization();
